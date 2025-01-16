8b56dc9416158ee8028981c950f4902825aeebee
lmodan <33545142+lmodan@users.noreply.github.com>
2025-01-16 10:33:09 +0100

    Delete wanos directory

--- wanos/d5989fe2-4cdd-45fa-be47-15718de71915/refconv.py
+++
@@ -1,8 +0,0 @@
-import LammpsDataDict as LD
-
-ld=LD.LammpsData()
-ld.readFile("fREF_polymer")
-#ld.masses=ld.masses[:-2] #disregard initiators
-del ld.masses[26]
-del ld.masses[27]
-ld.writeFile("fRef_pos", reorder=True)
